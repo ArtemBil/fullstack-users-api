5aa842a8795680cfd794d444ea5ed6759f37e87c
Artem Bilko <Art.Bilko14@gmail.com>
2024-02-19 09:16:53 +0200

    Add versioning to the API

--- client/src/services/getToken.ts
+++ client/src/services/getToken.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function getToken() {
-    const {data} = await axios.get<{success: boolean, token?: string}>('/api/token');
+    const {data} = await axios.get<{success: boolean, token?: string}>('/api/v1/token');
 
     return data;
 }
